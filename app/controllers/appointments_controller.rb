309b6616d650e61802f9477b66f3b5baba2c7ebf
jkivlen <jimmykivlen@gmail.com>
2021-01-31 18:52:58 -0600

    Done.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,30 @@
+class AppointmentsController < ApplicationController
+    def new
+        @appointment = Appointment.new
+    end
+
+    def create
+        @appointment = Appointment.new(appointment_params)
+        if @appointment.save
+            redirect_to appointment_path(@appointment)
+        else
+            render :new
+        end
+    end
+
+    def index
+        @appointments = Appointment.all
+    end
+
+    def show
+        @appointment = Appointment.find(params[:id])
+    end
+
+
+
+    private
+
+    def appointment_params
+        params.require(:appointment).permit(:appointment_datetime, :doctor_id, :patient_id)
+    end
+end
